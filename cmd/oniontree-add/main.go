31e8d0179eeeba209569dcca3251b1bdff1f9e22
Onion Limited <onionltd@protonmail.com>
2019-10-07 07:14:17 +0000

    oniontree-add: Add -tags option

--- cmd/oniontree-add/main.go
+++ cmd/oniontree-add/main.go
@@ -7,6 +7,7 @@ import (
 	"github.com/onionltd/oniontree-tools/pkg/oniontree"
 	"github.com/onionltd/oniontree-tools/pkg/types/service"
 	"os"
+	"strings"
 )
 
 func exitError(msg string) {
@@ -19,6 +20,7 @@ func main() {
 	name := flag.String("name", "", "Onion service name.")
 	description := flag.String("description", "", "Onion service description.")
 	urls := flag.String("url", "", "Onion service URL")
+	tags := flag.String("tags", "", "Onion service tags.")
 	flag.Parse()
 
 	s := service.Service{}
@@ -33,21 +35,27 @@ func main() {
 		exitError("id not specified")
 	}
 
-	wd, err := os.Getwd()
-	if err != nil {
-		panic(err)
-	}
-
 	b, err := yaml.Marshal(s)
 	if err != nil {
 		panic(err)
 	}
 
+	wd, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+
 	onionTree := oniontree.New(wd)
+
 	if err := onionTree.Add(*id, b); err != nil {
 		if err == oniontree.ErrIdExists {
 			exitError(err.Error())
 		}
 		panic(err)
+	}
+	if *tags != "" {
+		if err := onionTree.Tag(*id, strings.Split(*tags, ",")); err != nil {
+			panic(err)
+		}
 	}
 }
