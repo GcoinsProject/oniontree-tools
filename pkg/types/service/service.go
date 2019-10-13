c51640d508a3dd00ec270536b81f2e07798e5075
Onion Limited <onionltd@protonmail.com>
2019-10-13 12:41:12 +0000

    oniontree: Add new field PublicKey

--- pkg/types/service/service.go
+++ pkg/types/service/service.go
@@ -1,7 +1,8 @@
 package service
 
 type Service struct {
-	Name        string   `json:"name"`
-	Description string   `json:"description"`
-	URLs        []string `json:"urls"`
+	Name        string   `json:"name" yaml:"name"`
+	Description string   `json:"description" yaml:"description"`
+	URLs        []string `json:"urls" yaml:"urls"`
+	PublicKey   string   `json:"public_key" yaml:"public_key"`
 }
