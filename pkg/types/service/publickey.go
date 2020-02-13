a44de8c326f4a16b8dd6b313e5b2ce4f84508955
Onion Limited <onionltd@protonmail.com>
2020-02-13 14:09:02 +0000

    oniontree: Use `openpgp.ReadArmoredKeyRing`

--- pkg/types/service/publickey.go
+++ pkg/types/service/publickey.go
@@ -2,11 +2,8 @@ package service
 
 import (
 	"bytes"
-	"errors"
 	"fmt"
 	"golang.org/x/crypto/openpgp"
-	"golang.org/x/crypto/openpgp/armor"
-	"golang.org/x/crypto/openpgp/packet"
 )
 
 type PublicKey struct {
@@ -18,41 +15,24 @@ type PublicKey struct {
 }
 
 func ParseKey(key []byte) (PublicKey, error) {
-	block, err := armor.Decode(bytes.NewReader(key))
+	el, err := openpgp.ReadArmoredKeyRing(bytes.NewReader(key))
 	if err != nil {
 		return PublicKey{}, err
 	}
 
-	if block.Type != openpgp.PublicKeyType {
-		return PublicKey{}, errors.New("not a public key")
-	}
-
-	pktReader := packet.NewReader(block.Body)
-	pkt, err := pktReader.Next()
-	if err != nil {
-		return PublicKey{}, err
-	}
-
-	pk, ok := pkt.(*packet.PublicKey)
-	if !ok {
-		return PublicKey{}, errors.New("invalid public key")
-	}
-
-	pkt, err = pktReader.Next()
-	if err != nil {
-		return PublicKey{}, err
-	}
-
-	userID, ok := pkt.(*packet.UserId)
-	if !ok {
-		return PublicKey{}, errors.New("invalid user id data")
-	}
-
-	publicKey := PublicKey{
-		Value:       string(key),
-		ID:          pk.KeyIdString(),
-		Fingerprint: fmt.Sprintf("%X", pk.Fingerprint),
-		UserID:      userID.Id,
+	publicKey := PublicKey{}
+	for _, e := range el {
+		userID := ""
+		for _, ident := range e.Identities {
+			userID = ident.Name
+		}
+		pk := e.PrimaryKey
+		publicKey = PublicKey{
+			Value:       string(key),
+			ID:          pk.KeyIdString(),
+			Fingerprint: fmt.Sprintf("%X", pk.Fingerprint),
+			UserID:      userID,
+		}
 	}
 	return publicKey, nil
 }
